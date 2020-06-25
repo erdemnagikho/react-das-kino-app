7fe0581edd7a2268177aefc904edaa294129a8e0
Erdem Nayir <erdemnayir@gmail.com>
2020-06-25 16:43:12 +0300

    Grid Component Added

--- src/components/content/main-content/MainContent.js
+++ src/components/content/main-content/MainContent.js
@@ -2,17 +2,21 @@ import React, { useState } from 'react';
 import './MainContent.scss';
 import SlideShow from '../slide-show/SlideShow';
 import Paginate from '../paginate/Paginate';
+import Grid from '../grid/Grid';
 
 const MainContent = () => {
     const images = [
         {
-            url: 'https://images.unsplash.com/photo-1485846234645-a62644f84728?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1040&q=80'
+            url: 'https://images.unsplash.com/photo-1485846234645-a62644f84728?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1040&q=80',
         },
         {
             url: 'https://images.unsplash.com/photo-1478720568477-152d9b164e26?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
         },
         {
             url: 'https://images.unsplash.com/photo-1440404653325-ab127d49abc1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
+        },
+        {
+            url: 'https://images.unsplash.com/photo-1485846234645-a62644f84728?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1040&q=80'
         }
     ];
 
@@ -35,6 +39,7 @@ const MainContent = () => {
                     <Paginate currentPage={currentPage} totalPages={10} paginate={paginate} />
                 </div>
             </div>
+            <Grid images={images} />
         </div>
     );
 };
